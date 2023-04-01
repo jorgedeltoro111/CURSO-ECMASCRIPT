a9d35b8a0b1932ecab639ad27189a65d03fd36e2
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-04-01 12:11:18 -0600

    version 1

---
+++ src/ES6/12-Promesas-2.js
@@ -0,0 +1,32 @@
+const datos = [
+    {
+        name: 'Titanic', 
+        year: 2020
+    },
+    {
+        name: 'La princesa',
+        year: 2021
+    },
+    {
+        name: 'El estudiante',
+        year: 2015
+    }
+]
+const getDatos = () => {
+    return new Promise((resolve, reject) => {//creamos una nueva promesa que vamos a retornar
+        setTimeout(() => {//dado un cierto tiempo
+            resolve(datos);//llamamos a resolve y le pasamos los datos
+        }, 1500);
+    });
+}
+//primera forma de llamar a la promesa sin async ni await
+// getDatos()//llamar a funcion
+//     .then((datos) => console.log(datos))//y cuando se resuelva retornara lo que contiene resolve
+//     .catch((error)=> console.log(error));//mostramos el error
+
+//Segunda forma de hacer lo mismo pero con async y await
+async function data ()  {
+    const info = await getDatos();
+    console.log(info);
+}
+data();
